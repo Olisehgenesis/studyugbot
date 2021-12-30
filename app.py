4baed317d6eeec012610eef92f5163269470b7a9
oliseh <1901200195@muni.ac.ug>
2021-12-30 13:47:11 +0300

    changes

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 
-from flask import Flask,
-
+from flask import Flask
 app = Flask(__name__)
 from bot import *
 
 
 @app.route('/{}'.format(secret), methods=["POST"])
-def home():
-    return getdata
-
+def getdata():
+    update = request.get_json()
+    if "message" in update:
+        chat_id = update["message"]["chat"]["id"]
+        if "text" in update["message"]:
+            text = update["message"]["text"]
+            bot.sendMessage(chat_id, "From the web: you said '{}'".format(text))
+        else:
+            bot.sendMessage(chat_id, "From the web: sorry, I didn't understand that kind of message")
+    return "OK"
 
 if __name__=="__main__":
     app.run()
